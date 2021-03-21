3a6c990d9c871a11b4738be541908849242814f4
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-03-21 22:17:00 +0200

    add first example

---
+++ tasks/maximalPolindrome/solution.js
@@ -0,0 +1,42 @@
+module.exports.maximalPalindrome = maximalPalindrome;
+
+// "eye" is a palindrome
+// "noon" is a palindrome
+// "decaf faced" is a palindrome
+// "taco cat" is not a palindrome (backwards it spells "tac ocat")
+// "racecars" is not a palindrome (backwards it spells "sracecar")
+function maximalPalindrome(s) {
+    const sortedStr = sortString(s);
+
+    let start = [];
+    let end = [];
+    let joinChar = '';
+
+    for (let i = 0; i < sortedStr.length; ++i) {
+        let j = i + 1;
+        if (j < sortedStr.length) {
+            if (sortedStr[i] === sortedStr[j]) {
+                start.push(sortedStr[i]);
+                end.unshift(sortedStr[j]);
+
+                ++i;
+            } else {
+                joinChar = sortedStr[i];
+            }
+        } else {
+            joinChar = sortedStr[i];
+        }
+    }
+
+
+    return `${start.join('')}${joinChar}${end.join('')}`;
+}
+
+function sortString(s) {
+    return s.split('').sort().join('');
+}
+
+// TODO: add jest and move to tests
+// console.log(maximalPalindrome('abcabc'));
+// console.log(maximalPalindrome('gfagcaabdfdgfaabeabbage'));
+// console.log(maximalPalindrome('decaf faced'));
