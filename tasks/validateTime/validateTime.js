aae11d8a2c5d8d68880fb7a17416d90e7e11b1ab
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-03-24 12:19:04 +0200

    add task from eBay

---
+++ tasks/validateTime/validateTime.js
@@ -0,0 +1,18 @@
+function validTime(time) {
+    const timeParts = time.split(':');
+
+    return checkTimePart(timeParts[0], 24) && checkTimePart(timeParts[1], 60);
+}
+
+function checkTimePart(part, limit) {
+
+    const intPart = Number.parseInt(part);
+
+    if (Number.isNaN(intPart)) {
+        return false;
+    }
+
+    return intPart < limit;
+}
+
+// console.log(validTime("13:58"))
